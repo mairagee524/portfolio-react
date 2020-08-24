9b184a35d9c2a3fc368ea375db5774bd667b1374
mairagee524 <garciamaira524@yahoo.com>
2020-08-23 22:49:44 -0700

    refactored code for buttons

---
+++ src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function Button() {
+    return (
+        <div>
+            <button >Main call to action</button>
+            <button >Secondary action</button>
+        </div>
+    )      
+}
+
+export default Button;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,29 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import Button from '../Button';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Container from '@material-ui/core/Container';
 
 export default function Album(props) {
-  const classes = useStyles();
 
   return (
-    <React.Fragment>
-      <CssBaseline />
+    // <React.Fragment>
+    //   <CssBaseline />
       
-      <div style>
-        <Container maxWidth="sm">
+      <div>
+        {/* <Container maxWidth="sm"> */}
 
             <h1>Album Layout</h1>
             <p>Something short and leading about the collection below—its contents, the creator, etc.
             Make it short and sweet, but not too short so folks don&apos;t simply skip over it
             entirely.</p>
+
+            <Button />
             
-          <div className={classes.heroButtons}>
-            <Grid container spacing={2} justify="center">
-              <Grid item>
-                <Button variant="contained" color="primary">
-                  Main call to action
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button variant="outlined" color="primary">
-                  Secondary action
-                </Button>
-              </Grid>
-            </Grid>
-          </div>
-        </Container>
-        {props.children}
+          
+        {/* </Container>
+        {props.children} */}
       </div>
-    </React.Fragment>
+    //  </React.Fragment>
   );
 }

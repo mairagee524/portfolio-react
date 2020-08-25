6ed7e8814ac1449069a499b06ef421f17d030e61
mairagee524 <garciamaira524@yahoo.com>
2020-08-25 11:49:00 -0700

    added header and navbar code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,81 +1,115 @@
-import React, { Component } from 'react';
-
-class Header extends Component {
-  render() {
-    if(this.props.data){
-      console.log(this.props.data);
-      var name = this.props.data.name;
-      // var occupation = this.props.data.occupation;
-      // var description = this.props.data.description;
-      // var city = this.props.data.address.city;
-      var networks = this.props.data.social.map(function(network){
-        return <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      })
-    }
-
-    return (
-      <header id="home">
-
-      <nav id="nav-wrap">
-        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-        <ul id="nav" className="nav">
-          <li className="current">
-            <a className="smoothscroll" href="#home">Home</a>
-          </li>
-          <li>
-            <a className="smoothscroll" href="#about">About</a>
-          </li>
-	        <li>
-            <a className="smoothscroll" href="#resume">Resume</a>
-          </li>
-          <li>
-            <a className="smoothscroll" href="#portfolio">Works</a>
-          </li>
-          <li>
-            <a className="smoothscroll" href="#testimonials">Testimonials</a>
-          </li>
-          <li>
-            <a className="smoothscroll" href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-
-      <div className="row banner">
-        <div className="banner-text">
-          <h5>Hello, World.</h5>
-          <h1 className="responsive-headline">I'm {name}.</h1>
-          {/* <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
- */}
+import React from 'react';
+import HeroImage from './HeroImage';
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import HorizontalRule from './HorizontalRule';
+import Credit from './Credit';
 
 
-          <p className="intro-position">
-	   				<span>Full-Stack Developer</span>
-	   				<span>Bilingual</span> 
-            <span>Los Angeles Native</span> 
-
-	   			</p>
-
-	   			<a className="button stroke smoothscroll" href="#about" title="">More About Me</a>
-          <hr />
-          <ul className="social">
-            {networks}
-          </ul>
-        </div>
-      </div>
-
-      <p className="scrolldown">
-        <a className="smoothscroll" href="#about">
-          <i className="icon-down-circle"></i>
-          </a>
-      </p>
-   </header>
-    );
-  }
+function HomePage() {
+  return (
+    <div>
+      <HeroImage 
+        name="Hi, I'm Guillermo."
+        intro="I'm a front-end web developer based in the Bay Area."
+        className="HeroImage-img"
+      />
+      <Credit 
+        name="Tim Trad"
+        link="https://unsplash.com/@timtrad"
+      />
+      <About />
+      <HorizontalRule />
+      <Portfolio />
+      <HorizontalRule />
+      <Contact />
+    </div>
+  );
 }
 
-export default Header;
+export default HomePage;
+
+
+
+// import React, { Component } from 'react';
+
+// class Header extends Component {
+//   render() {
+//     if(this.props.data){
+//       console.log(this.props.data);
+//       var name = this.props.data.name;
+//       // var occupation = this.props.data.occupation;
+//       // var description = this.props.data.description;
+//       // var city = this.props.data.address.city;
+//       var networks = this.props.data.social.map(function(network){
+//         return <li key={network.name}>
+//           <a href={network.url}>
+//             <i className={network.className}></i>
+//           </a>
+//         </li>
+//       })
+//     }
+
+//     return (
+//       <header id="home">
+
+//       <nav id="nav-wrap">
+//         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+// 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+//         <ul id="nav" className="nav">
+//           <li className="current">
+//             <a className="smoothscroll" href="#home">Home</a>
+//           </li>
+//           <li>
+//             <a className="smoothscroll" href="#about">About</a>
+//           </li>
+// 	        <li>
+//             <a className="smoothscroll" href="#resume">Resume</a>
+//           </li>
+//           <li>
+//             <a className="smoothscroll" href="#portfolio">Works</a>
+//           </li>
+//           <li>
+//             <a className="smoothscroll" href="#testimonials">Testimonials</a>
+//           </li>
+//           <li>
+//             <a className="smoothscroll" href="#contact">Contact</a>
+//           </li>
+//         </ul>
+//       </nav>
+
+//       <div className="row banner">
+//         <div className="banner-text">
+//           <h5>Hello, World.</h5>
+//           <h1 className="responsive-headline">I'm {name}.</h1>
+//           {/* <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+//  */}
+
+
+//           <p className="intro-position">
+// 	   				<span>Full-Stack Developer</span>
+// 	   				<span>Bilingual</span> 
+//             <span>Los Angeles Native</span> 
+
+// 	   			</p>
+
+// 	   			<a className="button stroke smoothscroll" href="#about" title="">More About Me</a>
+//           <hr />
+//           <ul className="social">
+//             {networks}
+//           </ul>
+//         </div>
+//       </div>
+
+//       <p className="scrolldown">
+//         <a className="smoothscroll" href="#about">
+//           <i className="icon-down-circle"></i>
+//           </a>
+//       </p>
+//    </header>
+//     );
+//   }
+// }
+
+// export default Header;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,51 +1,191 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Background from "./background.jsx";
+import About from "./about.jsx";
+//import Test from "./test.jsx";
+import Projects from "./projects.jsx";
+import Contact from "./contact.jsx";
 
-function NavBar(props) {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/">
-      MG
-    </Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+import WOW from "wowjs";
 
+class Navbar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.about = React.createRef();
+		this.projects = React.createRef();
+		this.contact = React.createRef();
 
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li className="nav-item">
-          <Link
-            to="/"
-            className={
-              window.location.pathname === "/" || window.location.pathname === "/home"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/portfolio"
-            className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
-          >
-            Portfolio
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/contact"
-            className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  );
+		this.scrolling = this.scrolling.bind(this);
+	}
+
+	componentDidMount() {
+		new WOW.WOW().init();
+	}
+
+	navEffect() {
+		window.addEventListener("scroll", () => {
+			var navBar = document.getElementById("navbar");
+			var domRect = navBar.getBoundingClientRect();
+			var myBackground = document.getElementById("my-background");
+			var domBGRect = myBackground.getBoundingClientRect();
+
+			if (domRect.y <= -domRect.height) {
+				navBar.classList.add("fade-in-nav");
+			}
+			if (-domBGRect.height < domBGRect.top) {
+				navBar.classList.remove("fade-in-nav");
+			}
+		});
+	}
+
+	scrolling(instance) {
+		let node = document.getElementById(instance.current.props.id);
+		window.scrollTo({
+			top: node.offsetTop,
+			behavior: "smooth"
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<Background
+					ref={this.navEffect}
+					id="my-background"
+					aboutRef={this.about}
+					bounceIn={"wow bounceIn"}
+				/>
+				<nav
+					id="navbar"
+					className="navbar navbar-expand-lg navbar-light bg-light">
+					<div className="container">
+						<a
+							className="home-style navbar-brand"
+							onClick={() => {
+								window.scrollTo({
+									top: 0,
+									behavior: "smooth"
+								});
+							}}>
+							Back to Space
+						</a>
+						<button
+							className="navbar-toggler"
+							type="button"
+							data-toggle="collapse"
+							data-target="#navbarNavAltMarkup"
+							aria-controls="navbarNavAltMarkup"
+							aria-expanded="false"
+							aria-label="Toggle navigation">
+							<span className="navbar-toggler-icon" />
+						</button>
+						<div
+							className="collapse navbar-collapse"
+							id="navbarNavAltMarkup">
+							<div className="navbar-nav">
+								<a
+									onClick={() => {
+										this.scrolling(this.about);
+									}}
+									className="btn-style nav-item nav-link">
+									About
+								</a>
+								<a
+									onClick={() => {
+										this.scrolling(this.projects);
+									}}
+									className="btn-style nav-item nav-link">
+									Projects
+								</a>
+								<a
+									onClick={() => {
+										this.scrolling(this.contact);
+									}}
+									className="btn-style nav-item nav-link">
+									Contact
+								</a>
+							</div>
+						</div>
+					</div>
+				</nav>
+
+				<About
+					ref={this.about}
+					id="about-container"
+					bounceLeft={"wow bounceInLeft"}
+					fadeInLeft={"wow fadeInLeft"}
+					fadeInRight={"wow fadeInRight"}
+					fadeIn={"wow fadeIn"}
+					tada={"wow fadeIn"}
+				/>
+				<Projects
+					ref={this.projects}
+					id="my-projects"
+					fadeInRight={"wow fadeInRight"}
+					fadeIn={"wow fadeIn"}
+				/>
+				<Contact
+					ref={this.contact}
+					id="my-contact"
+					fadeInLeft={"wow fadeInLeft"}
+					shake={"wow pulse"}
+				/>
+			</div>
+		);
+	}
 }
 
-export default NavBar;
+export default Navbar;
+
+
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// function NavBar(props) {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//     <Link className="navbar-brand" to="/">
+//       MG
+//     </Link>
+//     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+//     <span className="navbar-toggler-icon"></span>
+//   </button>
+
+
+//     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+//       <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+//         <li className="nav-item">
+//           <Link
+//             to="/"
+//             className={
+//               window.location.pathname === "/" || window.location.pathname === "/home"
+//                 ? "nav-link active"
+//                 : "nav-link"
+//             }
+//           >
+//             Home
+//           </Link>
+//         </li>
+//         <li className="nav-item">
+//           <Link
+//             to="/portfolio"
+//             className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+//           >
+//             Portfolio
+//           </Link>
+//         </li>
+//         <li className="nav-item">
+//           <Link
+//             to="/contact"
+//             className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+//           >
+//             Contact
+//           </Link>
+//         </li>
+//       </ul>
+//     </div>
+//   </nav>
+//   );
+// }
+
+// export default NavBar;

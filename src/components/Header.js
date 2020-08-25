56015e372be4cb8d79d01c6c170fe054ad0c4735
mairagee524 <garciamaira524@yahoo.com>
2020-08-25 15:10:32 -0700

    added header and portfolio card

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,31 @@ function HomePage() {
       <Portfolio />
       <HorizontalRule />
       <Contact />
+
+      <h1>
+						Hello, I&apos;m Frank Izquierdo.
+						<br />
+						I&apos;m a Full Stack Web Developer.
+					</h1>
+					{/* offset can be cahnged in node modules wowjs default file */}
+					<button
+						className={"work-button " + this.props.bounceIn}
+						data-wow-offset="0"
+						onClick={() => {
+							let node = document.getElementById(
+								this.props.aboutRef.current.props.id
+							);
+							window.scrollTo({
+								top: node.offsetTop,
+								behavior: "smooth"
+							});
+						}}>
+						View my work
+					</button>
     </div>
+    
+
+
   );
 }
 

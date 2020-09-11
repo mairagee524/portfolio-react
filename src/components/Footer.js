3b414264141fae846fd832017036be4090f0ee75
mairagee524 <garciamaira524@yahoo.com>
2020-09-10 23:55:37 -0700

    removed logo and added footer statement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,53 +1,49 @@
 import React, { Component } from 'react';
 export default class Footer extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <footer>
-          <div className="row">
-              <div className="col-full">
+    render() {
+        return (
+        <footer>
+            <div className="row">
+                <div className="col-full">
+                    <h3>Let's connect!</h3>
 
-                  <div className="footer-logo">
-                      <a className="footer-site-logo" href="#0"><img src={resumeData.logo} alt="Homepage"/></a>
-                  </div>
+                    <ul className="footer-social">
+                        
+                        <li><a href="#0">
+                            <i className="im im-twitter" aria-hidden="true"></i>
+                            <span>Twitter</span>
+                        </a></li>
+                        <li><a href="#0">
+                            <i className="im im-instagram" aria-hidden="true"></i>
+                            <span>Github</span>
+                        </a></li>
+                        <li><a href="#0">
+                            <i className="im im-behance" aria-hidden="true"></i>
+                            <span>LinkedIn</span>
+                        </a></li>
+                        
+                    </ul>
+                        
+                </div>
+            </div>
 
-                  <ul className="footer-social">
-                      
-                      <li><a href="#0">
-                          <i className="im im-twitter" aria-hidden="true"></i>
-                          <span>Twitter</span>
-                      </a></li>
-                      <li><a href="#0">
-                          <i className="im im-instagram" aria-hidden="true"></i>
-                          <span>Github</span>
-                      </a></li>
-                      <li><a href="#0">
-                          <i className="im im-behance" aria-hidden="true"></i>
-                          <span>LinkedIn</span>
-                      </a></li>
-                    
-                  </ul>
-                      
-              </div>
-          </div>
+            <div className="row footer-bottom">
 
-          <div className="row footer-bottom">
+                <div className="col-twelve">
+                    <div className="copyright">
+                        <span>© Copyright Hola 2017</span> 
+                        <span>Design by <a href="https://www.styleshout.com/">styleshout</a></span>	
+                    </div>
 
-              <div className="col-twelve">
-                  <div className="copyright">
-                      <span>© Copyright Hola 2017</span> 
-                      <span>Design by <a href="https://www.styleshout.com/">styleshout</a></span>	
-                  </div>
+                    <div className="go-top">
+                    <a className="smoothscroll" title="Back to Top" href="#top"><i className="im im-arrow-up" aria-hidden="true"></i></a>
+                    </div>
+                </div>
 
-                  <div className="go-top">
-                  <a className="smoothscroll" title="Back to Top" href="#top"><i className="im im-arrow-up" aria-hidden="true"></i></a>
-                  </div>
-              </div>
+            </div>
+            <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
 
-          </div>
-         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
-
-  </footer>
-    );
-  }
+            </footer>
+        );
+    }
 }

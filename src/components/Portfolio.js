b1b1947aa6ff717fba3fe65d72d74e9922cc099f
mairagee524 <garciamaira524@yahoo.com>
2020-09-26 21:53:41 -0700

    added duplicate code to figure out why portfolio section is not working

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -19,8 +19,8 @@ export default class Porfolio extends Component {
           <h5>Portfolio</h5>
           <h1>Check Out Some of My Works</h1>
         </div> 
-        <div className="row">
-          <div id="portfolio-wrapper" className="block-1-2 port-col">
+        <div className="row container bd-grid">
+          <div id="portfolio-wrapper" className="port-col">
 
             {resumeData.portfolio.map(item => {
               return(
@@ -28,6 +28,7 @@ export default class Porfolio extends Component {
                   <div className="folio-item item-wrap">
                     {/* <div className="item-wrap"> */}
                       <img src={`${item.imgUrl}`} alt={`${item.alt}`}/>
+
                       <a href={`#${item.modal}`} className="overlay">
                         <div className="folio-item-table">
                           <div className="folio-item-cell">
@@ -39,6 +40,7 @@ export default class Porfolio extends Component {
                           </div>	                      	
                         </div>                    
                       </a>
+
                     {/* </div>	                */}
                   </div>
 
@@ -49,7 +51,7 @@ export default class Porfolio extends Component {
                     <div className="description-box">
                       <h4>{item.name} </h4>		      
                       <p>{item.description}</p>
-                      <div className="categories">{item.type}</div>			               
+                      <div className="categories">{item.type}</div>          
                     </div>
                     <div className="link-box">
                       <a href={`${item.repoUrl}`} >Github</a>
@@ -62,6 +64,86 @@ export default class Porfolio extends Component {
             })};
           </div>
         </div>  
+{/* 
+        <section class="work section" id="portfolio">
+          <h2 class="section-title">My Portfolio</h2>
+          <h4 class="portfolio-blurb">If on a desktop, feel free to hover over each image to learn what each application does and what technologies were used. If you are on a mobile device, feel free to click on the image itself for more information about each application. </h4>
+          <div class="container bd-grid">
+            <div class="work__img">
+              <div class="img-hover">
+                <img src="assets/img/soma-kitchen.png" alt=""/>
+              </div>
+              <div class="pic-info">
+                <div class="content">
+                  <h4>Soma Kitchen</h4>
+                  <p>A local restaurant in Downtown Los Angeles requested a new modern look for the site.</p>
+                  <p><span class="tech-used">Tech Used:</span> JavaScript, jQuery, Restful API, Node.js, Express.js, MySQL </p>
+                  <a href="https://github.com/zcorpuz/soma_kitchen" class="footer__icon">
+                    <i class='bx bxl-github bx-tada-hover bx-md'></i>
+                  </a>
+                  <a href="https://protected-everglades-09109.herokuapp.com/" class="footer__icon">
+                    <i class='bx bx-link bx-tada-hover bx-md'></i>
+                  </a>
+                </div>
+              </div>
+            </div>            
+          </div>
+        </section> */}
+
+        <section id="portfolio">
+
+          <div class="row section-intro">
+            <div class="col-twelve">
+
+              <h5>Portfolio</h5>
+              <h1>Check Out Some of My Works.</h1>
+
+              <p class="lead">Lorem ipsum Do commodo in proident enim in dolor cupidatat adipisicing dolore officia nisi aliqua incididunt Ut veniam lorem ipsum Consectetur ut in in eu do.</p>
+
+            </div>   		
+          </div> 
+
+          <div class="row portfolio-content">
+
+          <div class="col-twelve">
+
+            <div id="folio-wrapper" class="block-1-2 block-mob-full stack">
+
+              <div class="bgrid folio-item">
+                <div class="item-wrap">
+                  <img src="images/portfolio/liberty.jpg" alt="Liberty"/>
+                  <a href="#modal-01" class="overlay">
+                    <div class="folio-item-table">
+                      <div class="folio-item-cell">
+                        <h3 class="folio-title">Liberty</h3>	     					    
+                        <span class="folio-types">
+                            Graphic Design
+                        </span>
+                      </div>	                      
+                    </div>                    
+                  </a>
+                </div>	               
+              </div> 
+
+              <div id="modal-01" class="popup-modal slider mfp-hide">	
+                <div class="media">
+                  <img src="images/portfolio/modals/m-liberty.jpg" alt="" />
+                </div>      	
+
+                <div class="description-box">
+                  <h4>Liberty</h4>		      
+                  <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+                  <div class="categories">Web Development</div>			               
+                </div>
+                <div class="link-box">
+                  <a href="http://www.behance.net">Details</a>
+                    <a href="#" class="popup-modal-dismiss">Close</a>
+                  </div>		      
+                </div>
+              </div> 
+            </div>
+          </div> 
+        </section>
       </section> 
     ) 
   }
